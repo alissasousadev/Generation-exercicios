5ffe9bcd65355a89059e25bf0d8cd0db2fbb9628
Alissa Sousa <alissiasuelen@hotmail.com>
2025-12-02 19:06:50 -0300

    JS - Prática

---
+++ bloco-1/lista-3/exercicio-1.js
@@ -0,0 +1,27 @@
+const leia = require("readline-sync")
+
+// 1 - Escreva um algoritmo, que leia 2 números inteiros 
+// 2 - o primeiro número deve ser menor do que o segundo número. 
+// 3 - Caso contrário, deve ser exibida uma mensagem na tela informando que o intervalo é inválido e sair do programa. 
+// 4 - No intervalo informado, mostre na tela todos os números que são múltiplos de 3 e 5.
+
+let numero1 = leia.questionInt("Digite o primeiro numero do intervalo: ");
+let numero2 = leia.questionInt("Digite o segundo numero do intervalo: ");
+
+if (numero1 >= numero2) {
+    console.log("Intervalo é invalido");
+    return;
+}  
+
+let encontrou = false
+
+for (let i = numero1; i <= numero2; i++) {
+        if( i % 3 === 0 && i % 5 === 0) {
+            console.log(`${i} é múltiplo de 3 e 5`);
+            encontrou = true;
+        }
+    }
+
+if (!encontrou) {
+    console.log("Nenhum numero desse intervalo e multiplo de 3 e 5.")
+}
---
+++ bloco-1/lista-3/exercicio-6.js
@@ -0,0 +1,8 @@
+const leia = require("readline-sync")
+
+/*
+Um algoritmo, que leia números inteiros
+até que o número zero seja digitado. 
+Ao final, mostre na tela a média de todos os números digitados, que sejam múltiplos de 3. 
+*/
+
