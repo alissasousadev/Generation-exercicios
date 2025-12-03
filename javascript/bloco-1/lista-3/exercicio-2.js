83a1f00fac5bddce44e1701304184d2776b20668
Alissa Sousa <alissiasuelen@hotmail.com>
2025-12-03 12:03:24 -0300

    JS - Prática

---
+++ javascript/bloco-1/lista-3/exercicio-2.js
@@ -0,0 +1,12 @@
+const leia = require("readline-sync");
+
+//leia 10 números inteiros e mostre na tela 
+// quantos números são pares e quantos número são ímpares.
+
+let pares = 0;
+let impar = 0;
+
+for (let i = 1; i <= 10; i++) {
+    let numero = leia.questionInt(`Digite o ${i} numero:` );
+    if (i % 2 === 0 && )
+}
